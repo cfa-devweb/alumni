093a3715f389d135a3299f6b78382e965ea309ba
jeremyogerjs <79125306+jeremyogerjs@users.noreply.github.com>
2021-07-30 17:06:39 +1100

    add route web fix bug push

--- routes/web.php
+++ routes/web.php
@@ -12,13 +12,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// route pour l'accueil
 Route::get('/', function () {
     return view('welcome');
 });
 
+// route pour le profil
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+  
+
+
+
+// route pour l'application
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/profils', [MessageController::class,'show']) -> name('message');
+
+Route::get('/members', [MemberController::class, 'index']) ->name('promotion.membres');
+
+Route::get('actualites', [BlogPostController::class, 'index']);
+
+Route::get('actualites/{id}', [BlogPostController::class, 'show']);
