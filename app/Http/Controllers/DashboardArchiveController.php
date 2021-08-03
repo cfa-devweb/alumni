dbb0d026a5317ed9ad42720bd001e6dcf14356d6
Julien Walles <j.walles@cci-formation.nc>
2021-08-04 08:41:12 +1100

    Update DashboardArchiveController.php

--- app/Http/Controllers/DashboardArchiveController.php
+++ app/Http/Controllers/DashboardArchiveController.php
@@ -2,9 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog_post;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ArchiveController;
 
-class DashboardArchive extends Controller
+
+
+
+
+class DashboardarchiveController extends Controller
 {
-    //
+    public function index()
+    {  
+
+        $actualites = DB::table('blog_posts')
+            ->join('users', 'blog_posts.user_id', '=', 'users.id')
+            ->join('categories', 'blog_posts.categorie_id', '=', 'categories.id')
+            ->select('blog_posts.*', 'users.name AS username', 'categories.name AS category')
+            ->get();
+            // dd ($actualites);
+            return view('dashboardArchive', ['actualites'=>$actualites]);
+    }
+
+
 }
+        
+    
